4a16983527f452d8e569261d7a5cb3e4183713c8
Sakarin Radchapunya <sakarin.my@gmail.com>
2012-05-01 19:43:46 +0700

    Init Store Project with rails version 3.2.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '3d8f1e4311975debdaad817df6356705bd753134e72351055068aaa9f1b66a4311a0599ed7d861402ff6e3b28ed058bc6aa87ff26dbe94db1d564b26a680653c'
